4e3352c5d62c1c0ff2ac3c0ecc10b86b83799932
ArtemiusI <whuangx10@gmail.com>
2026-05-04 18:39:39 +0800

    fixed InfinityUI compatibility issue

--- shadowadept/lua/m_c0sasp__eeex.lua
+++ shadowadept/lua/m_c0sasp__eeex.lua
@@ -32,19 +32,3 @@ me_addSpellChoice_shadow_magic_exception = {
          ["kit_include"] = {C0SADEPT, C0SASORC, C0SABARD, C0SAMT, C0SACM, C0SAFM, C0SAFMT, C0SAFMC,},
     },
 }
-
-function me_canLearnSpell_shadow_magic_exception()
-  if id <= 0 or itemDescRightButtonText() ~= Infinity_FetchString(17104) then return itemDescRightButtonEnabled() end
-    local me_realClass = EEex_GameObject_Get(id).m_typeAI.m_Class
-    local me_realKit = EEex_Sprite_GetStat(EEex_GameObject_Get(id), 152)
-    local me_realAlignment = EEex_GameObject_Get(id).m_typeAI.m_Alignment
-for k, v in ipairs(me_addSpellChoice_shadow_magic_exception) do
-       if v.scroll == itemDesc.item.res then
-            for k2, v2 in ipairs(v.kit_include) do
-                if v2 == me_realKit then
-                return itemDescRightButtonEnabled()
-             end
-             end
-             end
-            end
-            end
